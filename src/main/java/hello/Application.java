9417cd5fdbcbdac3710357b527fd22caa46a39dd
Tom Fok <tom_fok@yahoo.com>
2018-07-01 14:06:23 -0700

    Added LinkedList import

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import java.util.LinkedList;
 
 @SpringBootApplication
 public class Application {
