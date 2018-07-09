f52c269906b4b9db934cb490c1b4d50ec467a678
Tom Fok <tom_fok@yahoo.com>
2018-07-08 22:50:51 -0700

    Merge branch 'branch2' into brach1
    # Conflicts:
    #	src/main/java/hello/Application.java

--- src/main/java/hello/Quote.java
+++ src/main/java/hello/Quote.java
@@ -1,12 +1,21 @@
 package hello;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+//import java.util.LinkedList;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Quote {
 
     private String type;
     private Value value;
+    private List<Integer> list =new LinkedList<>();
+    Map<String, Integer> map=new HashMap<>();
 
     public Quote() {
     }
